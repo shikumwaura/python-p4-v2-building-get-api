9c2a3ecbb58ff61f95e11e2837ac994de58b9e3e
Wanjiku <wanjikumwaura0304@gmail.com>
2025-09-29 00:41:06 +0300

    corrected error

--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-rom flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
